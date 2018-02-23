a94a3e98ba8c0507ad55b99b81bf02e47ef90fc9
Philip Van Raalte <phil.vanraalte@gmail.com>
2018-02-23 00:16:17 -0500

    improved styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route, Redirect, Link } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
 
 import RMS from "./assets/RMS_No_Slogan.png";
 
@@ -26,36 +27,52 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div id="site" className="site">
-          <Link to="/home">
-            <img
-              src={RMS}
-              alt="RMS"
-              style={{ height: 100, position: "absolute", left: 5, top: 5 }}
-            />
-          </Link>
-          <div className="site-content">
-            <Switch>
-              <Route exact path="/" component={Login} />
-              <Route exact path="/home" component={Home} />
-              <Route path="/customer/create" component={CreateCustomer} />
-              <Route path="/customer/edit/:id" component={EditCustomer} />
-              <Route path="/customer/:id" component={CustomerDetails} />
-              <Route path="/customer" component={Customer} />
+        <Switch>
+          <Route exact path="/" component={Login} />'
+          <Route>
+            <div id="site" className="site">
+              <Menu>
+                <Menu.Item
+                  children={
+                    <Link to="/home">
+                      <img
+                        src={RMS}
+                        alt="RMS"
+                        style={{ height: 30, padding: "5px 0 2px 5px", transform: "scale(1.6, 1.6)" }}
+                      />
+                    </Link>
+                  }
+                />
+                <Menu.Menu position="right">
+                  <Menu.Item as={Link} to="/home" name="Home" />
+                  <Menu.Item as={Link} to="/customer" name="Customers" />
+                  <Menu.Item as={Link} to="/employee" name="Employees" />
+                  <Menu.Item as={Link} to="/job" name="Jobs" />
+                </Menu.Menu>
+              </Menu>
+              <div className="site-content">
+                <Switch>
+                  <Route exact path="/home" component={Home} />
+                  <Route path="/customer/create" component={CreateCustomer} />
+                  <Route path="/customer/edit/:id" component={EditCustomer} />
+                  <Route path="/customer/:id" component={CustomerDetails} />
+                  <Route path="/customer" component={Customer} />
 
-              <Route path="/employee/create" component={CreateEmployee} />
-              <Route path="/employee/edit/:id" component={EditEmployee} />
-              <Route path="/employee/:id" component={EmployeeDetails} />
-              <Route path="/employee" component={Employee} />
+                  <Route path="/employee/create" component={CreateEmployee} />
+                  <Route path="/employee/edit/:id" component={EditEmployee} />
+                  <Route path="/employee/:id" component={EmployeeDetails} />
+                  <Route path="/employee" component={Employee} />
 
-              <Route path="/job/create" component={CreateJob} />
-              <Route path="/job/edit/:id" component={EditJob} />
-              <Route path="/job/:id" component={JobDetails} />
-              <Route path="/job" component={Job} />
-              <Redirect to="/" />
-            </Switch>
-          </div>
-        </div>
+                  <Route path="/job/create" component={CreateJob} />
+                  <Route path="/job/edit/:id" component={EditJob} />
+                  <Route path="/job/:id" component={JobDetails} />
+                  <Route path="/job" component={Job} />
+                  <Redirect to="/" />
+                </Switch>
+              </div>
+            </div>
+          </Route>
+        </Switch>
       </BrowserRouter>
     );
   }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,8 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Container, Button, Divider } from "semantic-ui-react";
 
+import RMS from "../assets/RMS_No_Slogan.png";
+
 export default props => (
-  <Container>
+  <Container style={{margin: "auto", textAlign: "center"}}>
+    <img
+      src={RMS}
+      alt="RMS"
+      style={{ height: 250, padding: "5px 0 2px 5px", display: "block", margin: "auto" }}
+    />
+    <h2>Capstone Project (Title TBD)</h2>
     <Button
       fluid
       as={Link}
