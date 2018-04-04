5b2d525b12da4b2b58ae1d4bf212a90058bc96e4
Philip Van Raalte <phil.vanraalte@gmail.com>
2018-04-04 18:43:55 -0400

    update dependencies

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,6 @@ import { withConsumer } from "components/Context";
 
 class Home extends Component {
   render() {
-    console.log(this.props);
     return (
       <Container>
         <h1>Home - {this.props.context.state.userDetails.username}</h1>
