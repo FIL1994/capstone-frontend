7efb36f7a05dcf6510350aac382fba10daa84d35
Philip Van Raalte <phil.vanraalte@gmail.com>
2018-04-11 19:15:00 -0400

    show usernames and roles in admin page

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
-import { Container, Button, Form, Icon, Segment } from "semantic-ui-react";
+import {
+  Container,
+  Button,
+  Form,
+  Icon,
+  Segment,
+  List
+} from "semantic-ui-react";
 import axios from "helpers/axios";
 import { URLS } from "constants/urls";
+import Avatar from "react-avatar";
 
 class Admin extends Component {
   state = {
@@ -17,27 +25,27 @@ class Admin extends Component {
 
   getUsers = async () => {
     const res = await axios.get(URLS.USER + "/all");
-    console.log("get users", res);
 
     this.setState({
       users: res.data || []
     });
   };
 
+  addUser = async () => {
+    await axios.post(URLS.USER, {
+      email: this.state.userName,
+      password: "password"
+    });
+
+    setTimeout(this.getUsers, 200);
+  };
+
   render() {
-    console.log(this.state);
 
     return (
       <Container>
         <Segment secondary>
-          <Form
-            onSubmit={() =>
-              axios.post(URLS.USER, {
-                email: this.state.userName,
-                password: "password"
-              })
-            }
-          >
+          <Form onSubmit={this.addUser}>
             <Form.Input inverted iconPosition="left" action size="small">
               <Icon name="at" />
               <input
@@ -51,7 +59,24 @@ class Admin extends Component {
           </Form>
         </Segment>
         <Segment>
-          {this.state.users.map(u => <div key={u.id}>{u.email}</div>)}
+          <List celled>
+            {this.state.users.map(u => (
+              <List.Item key={u.id}>
+                <Avatar round name={u.email} size={40} textSizeRatio={2.5} />
+                <span style={{ marginLeft: 20 }}>{u.email}</span>
+                <List
+                  horizontal
+                  size="small"
+                  style={{ marginLeft: 60, display: "block", marginBottom: 5 }}
+                >
+                  {u.roles &&
+                    u.roles.map(r => (
+                      <List.Item key={r.id}>{r.role}</List.Item>
+                    ))}
+                </List>
+              </List.Item>
+            ))}
+          </List>
         </Segment>
       </Container>
     );
